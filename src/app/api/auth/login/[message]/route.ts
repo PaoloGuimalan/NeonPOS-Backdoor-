731fd3ccc6352a6e2d639f90ea20f50fe172ddd4
neonpos <pauloportes.guimalan187@gmail.com>
2024-05-11 12:39:19 +0800

    fix: NextAPIRq now Next Req

--- src/app/api/auth/login/[message]/route.ts
+++ src/app/api/auth/login/[message]/route.ts
@@ -1,7 +1,7 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-import { NextResponse } from "next/server";
+// import type { NextApiRequest, NextApiResponse } from "next";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: NextApiRequest, { params }: { params: { message: string } }) {
+export async function GET(req: NextRequest, { params }: { params: { message: string } }) {
     console.log(params.message);
     return NextResponse.json({ status: true, message: `Next API test ${params.message}` });
     // res.send({ status: true, message: "Next API test" });
