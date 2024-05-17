35b203137f64b2c12f271d1fbd1b389a6b9da0b5
= <=>
2024-05-17 16:18:00 +0800

    feat: flattened permisions array in get specific user details

--- src/app/api/auth/getusers/[userID]/[accountID]/route.ts
+++ src/app/api/auth/getusers/[userID]/[accountID]/route.ts
@@ -5,8 +5,52 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest, { params }: { params: { userID: string, accountID: string } }) {
 	return await establishconnection().then(async () => {
-        return await UserAccount.find({ "createdBy.userID": params.userID, accountID: params.accountID }, { password: 0 }).then((result) => {
-            return NextResponse.json({ status: true, result: result });
+        return await UserAccount.aggregate([
+            {
+                $match: { "createdBy.userID": params.userID, accountID: params.accountID }
+            },
+			{
+				$lookup: {
+					from: "userpermissions",
+					// localField: "accountType",
+					// foreignField: "allowedUsers",
+					let: { accountType: "$accountType" },
+					pipeline: [
+						{
+							$match: {
+								$expr: {
+									$and: [
+										{ $in: ["$$accountType", "$allowedUsers"] },
+										{ $eq: [true, "$isEnabled"] },
+										{ $eq: [params.userID, "$from.userID"] }
+									]
+								}
+							}
+						}
+					],
+					as: "permissions"
+				}
+			},
+			{
+				$project: {
+					"password": 0,
+					"permissions._id": 0,
+					"permissions.permissionID": 0,
+					"permissions.allowedUsers": 0,
+					"permissions.isEnabled": 0,
+					"permissions.__v": 0
+				}
+			}
+        ]).then((result) => {
+            const userdetails = result.map((mp) => {
+                const flattenedpermissions = mp.permissions.map((mpp: any) => mpp.permissionType);
+
+                return {
+                    ...mp,
+                    permissions: flattenedpermissions
+                }
+            });
+            return NextResponse.json({ status: true, result: userdetails });
         }).catch((err) => {
             console.log(err);
             return NextResponse.json({ status: false, message: "Error fetching account" });
