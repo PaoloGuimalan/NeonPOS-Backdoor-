e7ea8e7ef7de9003fea65c3f927f4596123768a8
Paolo <pauloportes.guimalan187@gmail.com>
2024-05-25 19:48:18 +0800

    feat: close order v2 has been created and now consisting discount and status if renewed of initial order

---
+++ src/app/api/orders/closeorderV2/route.ts
@@ -0,0 +1,30 @@
+import establishconnection from "../../../_kernel/utils/establishconnection";
+
+import Order from '../../../_kernel/schemas/order';
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+    const rqst = await req.json();
+	return await establishconnection().then(async () => {
+        const orderID = rqst.orderID;
+        const discount = rqst.discount;
+        const amountreceived = rqst.amountreceived;
+        const userID = rqst.orderMadeBy.userID;
+        const deviceID = rqst.orderMadeBy.deviceID;
+        const isRenewed = rqst.isRenewed;
+        return await Order.updateOne({ orderID: orderID, "orderMadeBy.userID": userID, "orderMadeBy.deviceID": deviceID }, { receivedAmount: amountreceived, discount: discount, status: isRenewed ? "Renewed" : "Initial" }).then(async () => {
+            return await Order.find({ orderID: orderID, "orderMadeBy.userID": userID, "orderMadeBy.deviceID": deviceID }).then((result) => {
+                return NextResponse.json({ status: true, message: "Order has been closed", result: result });
+            }).catch((err) => {
+                console.log(err);
+                return NextResponse.json({ status: false, message: "Error fetching order details" });
+            })
+        }).catch((err: any) => {
+            console.log(err);
+            return NextResponse.json({ status: false, message: "There was a problem closing the order" });
+        })
+	}).catch((err) => {
+		console.log(err);
+        return NextResponse.json({ status: false, message: "Error establishing connection" });
+	})
+}
